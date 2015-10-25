d418468f4edeb2ea6991cd004aa9d9fbfc9af959
taochengwei <staugur@vip.qq.com>
2015-10-26 00:02:36 +0800

    resolve redis error

--- Core/sdp_redis.py
+++ Core/sdp_redis.py
@@ -32,14 +32,14 @@ class RedisObject():
   def set(self, k, v):
     if k == None or v == None:
       print "parameter error, key or value is none."
-      return 1
+      exit(1)
     if self.redis_object.exists(k):
       print "%s exists, quit." % k
-      return 1
+      exit(1)
     else:
       if self.redis_object.get(k) == v:
         print "%s exist, but equal %s, will quit." %(k,v)
-        return 1
+        exit(1)
       else:
         self.redis_object.set(k,v)
         self.redis_object.save()
@@ -48,7 +48,7 @@ class RedisObject():
   def get(self, k):
     if k == None:
       print "key(%s) get error." % k
-      return 1
+      exit(1)
     return self.redis_object.get(k)
 
   def mset(self):
@@ -57,10 +57,10 @@ class RedisObject():
   def mget(self):
     pass
 
-connect=('127.0.0.1', 6379, 0, None)
+connect=('127.0.0.1', 6379, 1, None)
 test=RedisObject(connect)
-print test.ping()
-print test.keys()
-print test.set('name','taochengwei')
-#print test.get()
+print "Connect:%s" % test.ping()
+#print test.keys()
+#print test.set('name','taochengwei')
+print test.get('name')
 
