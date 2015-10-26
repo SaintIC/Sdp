9d62ff7f4f930d5c3aae721be7bcef6bf50ebc72
陶成伟 <staugur@vip.qq.com>
2015-10-26 15:09:26 +0800

    tmp

--- Core/sdp_mysql.py
+++ Core/sdp_mysql.py
@@ -7,22 +7,29 @@ __version__ = 'sdp1.1'
 
 try:
   import MySQLdb as mysql
-except ImportError as errmsg1:
-  import mysql
-except ImportError as Errmsg2:
-  print __file__, "import MySQLdb and mysql-connector-python modules failed, because %s,%s" %(Errmsg1, Errmsg2)
+except ImportError as errmsg:
+  print __file__, "fail: %s" % errmsg
   exit(1)
 
 class MySQLObject():
   def __init__(self, conn):
-    '''析构函数__init__需要两个参数，除了self，还需要传给类MySQLObject一个conn tuple，包含连接的五个元素。'''
     if not isinstance(conn, (tuple)):
-      raise TypeError('Bad Error Type, ask a tuple.')
+      raise TypeError('Bad Error Type, ask a tuple, (host,port,db,user,password)')
     if len(conn) == 5:
-      self.redis_object = mysql.connect(host=conn[0], port=conn[1], db=conn[2], user=conn[3], password=conn[4])
+      self.mysql_object = mysql.connect(host=conn[0], port=conn[1], db=conn[2], user=conn[3], passwd=conn[4])
     else:
       print 'Entry error, requires five elements.'
       exit(2)
 
-connect=('127.0.0.1', 6379, 1, 'root', 'passwd)'
+  def insert(self, sql):
+    #check sql
+    op=self.mysql_object.cursor()
+    rs=op.execute(sql)
+    op.close()
+    self.mysql_object.close()
+    return rs
+
+connect=('127.0.0.1', 33061, 'sdp', 'sdp', 'passwd')
 test=MySQLObject(connect)
+print test.insert('show tables;')
+
