a1968f1876daaf41412305920e9675280f8836c4
taochengwei <staugur@vip.qq.com>
2015-12-06 21:40:42 +0800

    spmc init

--- Core/Error.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*- coding=utf8 -*-
-__date__ = '2015.11.25'
-__doc__ = '''Error handler'''
-
-class SdpError(Exception):
-    pass
-
--- Core/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = '1.1.3'
-__all__ = ['Handler', 'Public', 'Config', 'Error', 'Log']
